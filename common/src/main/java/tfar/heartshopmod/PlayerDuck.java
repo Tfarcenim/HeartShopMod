b4af8e36bb3e045cce7da2520d18f502baf4e9f0
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-11-16 22:42:42 -0500

    swap to health

--- common/src/main/java/tfar/heartshopmod/PlayerDuck.java
+++ common/src/main/java/tfar/heartshopmod/PlayerDuck.java
@@ -1,9 +1,25 @@
 package tfar.heartshopmod;
 
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.UUID;
+
 public interface PlayerDuck {
 
-    int getHeartCurrency();
-    void setHeartCurrency(int hearts);
+    UUID uuid = UUID.fromString("547f2d4a-8238-40ff-8cc1-b9d69883bf9b");
+
+    default int getHeartCurrency() {
+        AttributeModifier modifier = getPlayer().getAttribute(Attributes.MAX_HEALTH).getModifier(uuid);
+        return modifier != null ? (int) (modifier.getAmount() / 2) : 0;
+    }
+
+    default void setHeartCurrency(int hearts) {
+        getPlayer().getAttribute(Attributes.MAX_HEALTH).removePermanentModifier(uuid);
+        getPlayer().getAttribute(Attributes.MAX_HEALTH).addPermanentModifier(create(hearts * 2));
+    }
+
     default void reset() {
         setHeartCurrency(0);
     }
@@ -12,4 +28,11 @@ public interface PlayerDuck {
         setHeartCurrency(getHeartCurrency() + hearts);
     }
 
+    default Player getPlayer() {
+        return (Player) this;
+    }
+
+    static AttributeModifier create(double health) {
+        return new AttributeModifier(uuid,"HeartShopMod",health, AttributeModifier.Operation.ADDITION);
+    }
 }
--- common/src/main/java/tfar/heartshopmod/mixin/PlayerMixin.java
+++ common/src/main/java/tfar/heartshopmod/mixin/PlayerMixin.java
@@ -1,53 +1,10 @@
 package tfar.heartshopmod.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import tfar.heartshopmod.PlayerDuck;
 
 @Mixin(Player.class)
-abstract class PlayerMixin extends LivingEntity implements PlayerDuck {
+abstract class PlayerMixin implements PlayerDuck {
 
-    @SuppressWarnings("all") //shush
-    private static final EntityDataAccessor<Integer> HEART_CURRENCY = SynchedEntityData.defineId(Player.class, EntityDataSerializers.INT);
-
-    protected PlayerMixin(EntityType<? extends LivingEntity> $$0, Level $$1) {
-        super($$0, $$1);
-    }
-
-    @Inject(method = "addAdditionalSaveData",at = @At("RETURN"))
-    private void addExtra(CompoundTag tag, CallbackInfo ci) {
-        tag.putInt("heart_currency", getHeartCurrency());
-    }
-
-    @Inject(method = "readAdditionalSaveData",at = @At("RETURN"))
-    private void readExtra(CompoundTag tag, CallbackInfo ci) {
-        setHeartCurrency(tag.getInt("heart_currency"));
-    }
-
-
-    @Inject(method = "defineSynchedData",at = @At("RETURN"))
-    private void registerCustom(CallbackInfo ci) {
-        this.entityData.define(HEART_CURRENCY, 0);
-    }
-
-    @Override
-    public int getHeartCurrency() {
-        return entityData.get(HEART_CURRENCY);
-    }
-
-    @Override
-    public void setHeartCurrency(int hearts) {
-        entityData.set(HEART_CURRENCY,hearts);
-    }
 }
